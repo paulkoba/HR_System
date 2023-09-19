66a8e1984ec1cad96a0b5f0573424eb26357a925
Nikita Vertelko <vernik03@gmail.com>
2023-09-20 01:46:18 +0300

    add first menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import mysql.connector 
 from mysql.connector import Error
 from prettytable import PrettyTable, from_db_cursor
@@ -26,7 +27,7 @@ connection = create_connection(IP, USERNAME, PASSWORD, "spf_management")
 
 bot = telebot.TeleBot(TOKEN)
 
-def QueryDB(connection, query, parameters):
+def query_db(connection, query, parameters):
     cursor = connection.cursor()
 
     try:
@@ -41,7 +42,7 @@ def QueryDB(connection, query, parameters):
     except Error as e :
         print(f"The error '{e}' occurred")
 
-def FormatTableFromQueryResult(response, description, **kwargs):
+def format_table_form_query_result(response, description, **kwargs):
     table = PrettyTable(**kwargs)
     table.align = "l"
     table.field_names = [col[0] for col in description]
@@ -49,14 +50,45 @@ def FormatTableFromQueryResult(response, description, **kwargs):
         table.add_row(row)
     return str(table)
 
+def get_table_to_print(connection, query, parameters, **kwargs):
+    description, response = query_db(connection, query, parameters)
+    return '`' + format_table_form_query_result(response, description, **kwargs) + '`'
+
+def make_main_menu():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Створити таску")
+    item2=types.KeyboardButton("Переглянути таски")
+    item3=types.KeyboardButton("Список членів СПФ")
+    item4=types.KeyboardButton("Звіт про помилку")
+    markup.add(item1, item2)
+    markup.add(item3, item4)
+    return markup
+
+def menu_handler(message):
+    if message.text == 'Створити таску':
+        bot.send_message(message.chat.id,'Иди работай')
+    elif message.text == 'Переглянути таски':
+        bot.send_message(message.chat.id,'Целая куча, иди работай')
+    elif message.text == 'Список членів СПФ':
+        bot.send_message(message.chat.id, get_table_to_print(connection, "SELECT * FROM members", None), parse_mode='MarkdownV2')
+    elif message.text == 'Звіт про помилку':
+        bot.send_message(message.chat.id,'Ничё не работает')
+    else:
+        bot.send_message(message.chat.id,'Не зрозумів')
+
+def text_message_handler(message):
+    if message.chat.type == 'private':
+        menu_handler(message)
+    
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    markup=make_main_menu()
+    bot.send_message(message.chat.id,'Оберіть дію', reply_markup=markup)
+  
 
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    description, response = QueryDB(connection, "SELECT * FROM members", None)
-    formatted_table = FormatTableFromQueryResult(response, description)
-    bot.reply_to(message, '`' + formatted_table + '`', parse_mode='MarkdownV2')
+def reply_to_message(message):
+    text_message_handler(message)
 
 bot.infinity_polling()
